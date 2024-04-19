29ada84993a87e5c85102589935b1d138540626a
Anonymous21-03 <rahul.pardasani03@gmail.com>
2024-04-19 17:32:21 +0530

    First

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,12 +1,23 @@
-// App.js
 import React from 'react'
-import Home from './Components/Home'
+import Home from './Pages/Home'
+import Navbar from './Components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './Pages/About'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
 
-function App () {
+
+function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ my-app1/src/Pages/Register.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Register.css';
+
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      // Send a POST request to the server with the registration data
+      const response = await axios.post('/api/register', {
+        email,
+        password,
+        username,
+        firstName,
+        lastName,
+      });
+      console.log(response.data.message); // Log the response message
+    } catch (error) {
+      console.error('Registration failed:', error.response.data.error);
+    }
+  };
+
+  return (
+    <div className="body-page">
+      <div className="body-container">
+        <div className="illustration-container">
+          <img src="/doodle.jpeg" alt="Registration Illustration" />
+        </div>
+        <div className="body-content">
+          <h2>Register</h2>
+          <form onSubmit={handleRegister}>
+            <div className="form-row">
+              <div className="form-group">
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                  className="single-line-input"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                  className="single-line-input"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="single-line-input"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="single-line-input"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                className="single-line-input"
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="body-button">
+                Register
+              </button>
+              <a href="#">Sign In</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
